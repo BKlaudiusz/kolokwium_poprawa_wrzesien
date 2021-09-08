ba9e55c30232b3ceb552634a05dbbd90f0fbf43e
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-09-08 17:12:39 +0200

    whenCantHeatOvenShoudReturnExeption

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -1,18 +1,63 @@
 package edu.iis.mto.oven;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
 
+    @Mock
+    HeatingModule heatingModule;
+
+    @Mock
+    Fan fan;
+
+    List<ProgramStage> programStageList = List.of(
+            ProgramStage.builder()
+                    .withStageTime(50)
+                    .withHeat(HeatType.GRILL)
+                    .withTargetTemp(100)
+                    .build(),
+            ProgramStage.builder()
+                    .withStageTime(20)
+                    .withHeat(HeatType.HEATER)
+                    .withTargetTemp(100)
+                    .build()
+    );
+
+    BakingProgram bakingProgram;
+
+    Oven oven;
+
+    @BeforeEach
+    void setUp() {
+        oven = new Oven(heatingModule, fan);
+        bakingProgram = BakingProgram.builder()
+                .withStages(programStageList)
+                .withInitialTemp(100)
+                .build();
+    }
     @Test
-    void itCompiles() {
-        assertThat(true, equalTo(true));
+    void whenCantHeatOvenShoudReturnExeption() throws HeatingException {
+        doThrow(HeatingException.class).when(heatingModule).heater(any(HeatingSettings.class));
+        Assertions.assertThrows(OvenException.class, () -> oven.runProgram(bakingProgram));
+
     }
 
 }
